50322879a522882426d944c4de6aee70fb689aea
Your Name <you@example.com>
2023-11-08 19:49:10 +0500

    Updated

--- database/__init__.py
+++ database/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_URL = 'sqlite:///database.db'
+SQLALCHEMY_URL = 'postgresql://postgres:admin@db/postgres/'
 engine = create_engine(SQLALCHEMY_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
